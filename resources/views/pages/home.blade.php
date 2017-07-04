1598298cb184dd3c0607046942ed05194a7f3b9a
nkear003 <nkearney757@gmail.com>
2017-07-05 00:44:46 +0200

    working on layout

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,16 +1,13 @@
 @extends('layouts.main')
 
-
 @section('content')
 
-<div class="container d-flex justify-content-center">
-    <div class="row">
-        <div class="col">
-            
-        </div>
-        <div class="col">
-            @yield('')
-        </div>
+<div class="row">
+    <div class="col">
+        @yield('post_thumbnail')
+    </div>
+    <div class="col">
+        <img src="{{URL::asset('images/strange2.png')}}" alt="Strange Matter Poster">
     </div>
 </div>
 
