c3171aceb42a7b6f706c97f44586d436facf26c5
nkear003 <nkearney757@gmail.com>
2017-07-05 00:42:42 +0200

    fixed some syntax errors

--- resources/views/single.blade.php
+++ resources/views/single.blade.php
@@ -1,7 +1,14 @@
-@extends('layouts/releases')
+@extends('layouts.main')
 
 @section('content')
 
-{{ HTML::image('images/strange2.png') }}
+<div class="row ">
+    <div class="col">
+        <img src="{{URL::asset('images/strange2.png')}}" alt="Strange Matter Poster">
+    </div>
+    <div class="col">
+        <img src="{{URL::asset('images/strange2.png')}}" alt="Strange Matter Poster">
+    </div>
+</div>
 
 @stop
