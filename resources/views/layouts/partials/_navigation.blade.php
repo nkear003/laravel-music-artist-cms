3ad9794819f5bea8928be98ee69f792bb543cf23
nkear003 <nkearney757@gmail.com>
2017-07-04 15:52:26 +0200

    centered nav bar

--- resources/views/layouts/partials/_navigation.blade.php
+++ resources/views/layouts/partials/_navigation.blade.php
@@ -1,8 +1,8 @@
-<nav class="navbar navbar-toggleable-md navbar-light bg-faded">
+<nav class="navbar navbar-toggleable-sm navbar-light">
   <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
-  <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+  <div class="collapse navbar-collapse d-flex justify-content-center" id="navbarNavAltMarkup">
     <div class="navbar-nav">
       <a class="nav-item nav-link active" href="#">//HOME//<span class="sr-only">(current)</span></a>
     </div>
