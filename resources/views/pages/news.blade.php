4bb8e2d03a10d4eddebbbc2f3dd43c56c3f14046
nkear003 <nkearney757@gmail.com>
2017-08-30 11:29:19 +0200

    created news blade

---
+++ resources/views/pages/news.blade.php
@@ -0,0 +1,12 @@
+@extends('main')
+
+
+@section('content')
+
+<div class="row">
+    <div class="col">
+        <img src="{{asset($wm->path)}}" class="rounded-circle">
+    </div>
+</div>
+
+@endsection
