f64b2dfd1c59bf8cba11bcffcf5fc63fe6860036
nkear003 <nkearney757@gmail.com>
2017-08-30 11:28:15 +0200

    updated adminnav with dropdowns

--- resources/views/partials/_adminbar.blade.php
+++ resources/views/partials/_adminbar.blade.php
@@ -5,11 +5,17 @@
     </button>
 
     <div class="collapse navbar-collapse d-flex justify-content-center" id="navbarSupportedContent">
-        <ul class="navbar-nav">
-            <li class="nav-item"><a href="{{url('/')}}" class="nav-link {{ Request::is('admin') ? "active" : "" }}">Home</a></li> 
-            <li class="nav-item"><a class="nav-link" href="{{url('/posts')}}">Posts Index</a></li>
+        <div class="navbar-nav">
+            <div class="nav-item"><a href="{{url('/')}}" class="nav-link {{ Request::is('admin') ? "active" : "" }}">Home</a></div> 
             <div class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Files</a>
+                <a href="#" class="nav-link dropdown-toggle" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Posts</a>
+                <div class="dropdown-menu">
+                    <a href="{{route('posts.create')}}" class="dropdown-item">Create Post</a>
+                    <a href="{{url('/posts')}}" class="dropdown-item">Posts Index</a>
+                </div>   
+            </div>
+            <div class="nav-item dropdown">
+                <a href="#" class="nav-link dropdown-toggle" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Files</a>
                 <div class="dropdown-menu">
                     <a href="{{route('files.create')}}" class="dropdown-item">Upload File</a>
                     <a href="{{route('files.index')}}" class="dropdown-item">Files Index</a>
@@ -23,7 +29,7 @@
                     <a class="dropdown-item {{ Request::is('wm') ? "active" : ""}}" href="{{url('/wm')}}">WM</a>
                 </div>
             </div>
-            <li class="nav-item"><a href="#" id="show_user_nav" class="nav-link">Show User Nav</a></li>
-        </ul>
+            <div class="nav-item"><a href="#" id="show_user_nav" class="nav-link">Show User Nav</a></div>
+        </div>
     </div>
 </nav>
