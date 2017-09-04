2967b277db192f5cade4e2d515bb9b25124bb7ce
nkear003 <nkearney757@gmail.com>
2017-09-04 14:29:11 +0200

    two rows w 3 columns each display for news page

--- resources/views/pages/news.blade.php
+++ resources/views/pages/news.blade.php
@@ -3,9 +3,29 @@
 
 @section('content')
 
+<!-- round 1 -->
 <div class="row">
     <div class="col">
-        <img src="{{asset($wm->path)}}" class="rounded-circle">
+        <img src="{{asset($wm)}}" class="rounded-circle">
+    </div>
+    <div class="col">
+        <img src="{{asset($release)}}">
+    </div>
+    <div class="col">
+        <img src="{{asset($poster)}}">
+    </div>
+</div>
+
+<!--round2-->
+<div class="row">
+    <div class="col">
+        <img src="{{asset($release2)}}">
+    </div>
+    <div class="col">
+        <img src="{{asset($wm2)}}" class="rounded-circle">
+    </div>
+    <div class="col">
+        <img src="{{asset($poster2)}}">
     </div>
 </div>
 
