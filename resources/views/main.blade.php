1bd4930d58cd0e8b650a6af1bb57f216dd488971
nkear003 <nkearney757@gmail.com>
2017-09-08 16:07:08 +0200

    remade user nav

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -1,34 +1,18 @@
 <!DOCTYPE html>
 <html lang="en">
     @include('partials._head')
-    
     <body>
-
-        @include('partials._adminbar')
-
         @include('partials._navbar')
-
         <!-- Content -->
         <div class="container-fluid">
-
             <div class="row">
-
                 <div class="offset-1 col-10">
-
                     @include('partials._messages')
-                    
-                    
-
                     @yield('content')
-
                 </div>
-
             </div>
-
         </div>
-
         @include('partials._footer')
     <script src="{{asset('js/script.min.js')}}"></script>
     </body>
-    
 </html>
