aaa65693cfc9c2d704f6c0ab9b9fe16347de3e29
nkear003 <nkearney757@gmail.com>
2017-09-18 19:16:31 +0200

    commit for deployment

--- resources/views/pages/news.blade.php
+++ resources/views/pages/news.blade.php
@@ -1,18 +1,23 @@
 @extends('main')
 
-
 @section('content')
 
 <div class="row">
     @for($i = 0; $i < count($files) - 1; $i+=3)
-    <hr>
+
     <div class="col">
-      <img src="{{asset($files[$i]->path)}}"
-        class="{{ $files[$i]->category_id == 3 ? 'rounded-circle' : ''}} mb-3">
-      <img src="{{asset($files[$i+1]->path)}}"
-        class="{{ $files[$i+1]->category_id == 3 ? 'rounded-circle' : ''}} mb-3">
-      <img src="{{asset($files[$i+2]->path)}}"
-        class="{{ $files[$i+2]->category_id == 3 ? 'rounded-circle' : ''}} mb-3">
+      <a href="{{ $files[$i]->category_id == 3 ? url('/wm') : '#' }}">
+        <img src="{{asset($files[$i]->path)}}"
+          class="{{ $files[$i]->category_id == 3 ? 'rounded-circle' : ''}} mb-3">
+      </a>
+      <a href="{{ $files[$i+1]->category_id == 3 ? url('/wm') : '#' }}">
+        <img src="{{asset($files[$i+1]->path)}}"
+          class="{{ $files[$i+1]->category_id == 3 ? 'rounded-circle' : ''}} mb-3">
+      </a>
+      <a href="{{ $files[$i+2]->category_id == 3 ? url('/wm') : '#' }}">
+        <img src="{{asset($files[$i+2]->path)}}"
+          class="{{ $files[$i+2]->category_id == 3 ? 'rounded-circle' : ''}} mb-3">
+      </a>
     </div>
     @endfor
 </div>
--- resources/views/pages/wm.blade.php
+++ resources/views/pages/wm.blade.php
@@ -3,22 +3,18 @@
 @section('title', ' | Weekend Mixtapes')
 
 @section('content')
-    
-<div class="row">
-    <h1>This is the Weekend Mixtapes page.</h1>
-</div>
-    
-<div class="row">
 
+<div class="row">
     @for($i = 0; $i < count($posts); $i++)
     <div class="col">
         <img class="img-fluid rounded-circle img-thumbnail" src="{{$posts[$i]->path}}" alt="">
-    </div>    
+    </div>
     @if($i == 2 || $i == 5)
 </div>
+
 <div class="row mt-3">
     @endif
-    @endfor       
+    @endfor
 </div>
-    
+
 @endsection
